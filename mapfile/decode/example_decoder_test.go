43d12ea5852d1404693b2f751597d32f6a7a4c84
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2016-03-17 14:56:15 +0000

    Added an example for the mapfile decoder.

---
+++ mapfile/decode/example_decoder_test.go
@@ -0,0 +1,48 @@
+package decode_test
+
+import (
+	"fmt"
+	"github.com/geo-data/mapfile/mapfile/decode"
+	"log"
+)
+
+func ExampleDecoder() {
+	mapfile := `
+MAP
+  NAME hello
+  STATUS ON
+  EXTENT 0 0 4000 3000
+  SIZE 400 300
+  IMAGECOLOR 200 255 255
+  LAYER
+    NAME 'credits'
+    STATUS DEFAULT
+    TYPE ANNOTATION
+    FEATURE
+      POINTS
+        200 150
+      END
+      TEXT 'Hello world.  Mapserver rocks.'
+    END
+    CLASS
+      LABEL
+        TYPE BITMAP
+        COLOR 0 0 0
+      END
+    END
+  END
+END`
+
+	dec, err := decode.DecodeString(mapfile)
+	if err != nil {
+		log.Fatalf("DecodeString() failed: %s", err)
+	}
+
+	map_, err := dec.Map()
+	if err != nil {
+		log.Fatalf("Decoder.Map() failed: %s", err)
+	}
+
+	fmt.Println(string(map_.Name))
+	// Output: hello
+}
